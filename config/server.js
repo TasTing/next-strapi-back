658e33065aa14935eae435ce7c8aa66fbe7dd0d1
Ting <badonlineqq@gmail.com>
2020-10-12 18:25:42 +1100

    'first'

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a2af80b8760f2516c89ff44910cfbb9a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e029a938-f535-43be-b657-1cecdb846ab6'
+};
