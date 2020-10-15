76bfa389dc5dca64d8cb9be499cf58b510a35b8a
Ting <badonlineqq@gmail.com>
2020-10-15 22:32:21 +1100

    added customized return json to category.js.
    back to where it original was

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -1,4 +1,4 @@
-const { sanitizeEntity } = require('strapi-utils');
+const {sanitizeEntity} = require('strapi-utils');
 
 module.exports = {
   /**
@@ -8,16 +8,9 @@ module.exports = {
    */
 
   async findOne(ctx) {
-    const { slug } = ctx.params;
+    const {slug} = ctx.params;
 
-    const entity = await strapi.services.category.findOne({ slug },['articles','articles.author','articles.author.picture']);
-    return sanitizeEntity(entity, { model: strapi.models.category });
+    const entity = await strapi.services.category.findOne({slug},);
+    return sanitizeEntity(entity, {model: strapi.models.category});
   },
-
-  async find (ctx) {
-    const { slug } =ctx.params;
-
-    const entity = await strapi.services.category.find({ slug },['articles','articles.author','articles.author.picture','articles.image']);
-    return sanitizeEntity(entity, { model: strapi.models.category });
-  }
 };
